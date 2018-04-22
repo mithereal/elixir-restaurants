3da8435b9d72f6b0f77380603f91fb660a13ae7c
Jason Clark <mithereal@gmail.com>
2018-04-21 17:20:17 -0700

    flesh out supervisor

--- lib/restaurant.supervisor.ex
+++ lib/restaurant.supervisor.ex
@@ -15,4 +15,38 @@ end
 def start(hash) do
   Supervisor.start_child(__MODULE__, [ hash ])
 end
+
+
+def stop_quote(hash) do
+  case Registry.lookup(@registry_name,  hash) do
+    [] -> :ok
+    [{pid, _}] ->
+      Process.exit(pid, :shutdown)
+      :ok
+  end
+end
+
+def whereis(hash) do
+
+case Registry.lookup(@registry_name, hash) do
+  [{pid, _}] -> pid
+  [] -> nil
+end
+end
+
+def find_or_create_process(hash)  do
+   if process_exists?(hash) do
+     {:ok, hash}
+   else
+     hash |> start
+   end
+end
+
+def process_exists?(hash)  do
+  case Registry.lookup(@registry_name, hash) do
+  case Registry.lookup(@registry_name, hash) do
+    [] -> false
+    _ -> true
+  end
+end
 end
